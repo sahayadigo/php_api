f9ca4a09af03c4238dcadfe2cbce94768c8bcb62
Digo <51820422+sahayadigo@users.noreply.github.com>
2023-02-20 19:19:41 +0530

    Update create.php

--- items/create.php
+++ items/create.php
@@ -21,14 +21,68 @@ if(!empty($data->name) && !empty($data->description) &&
     // $items->created = date('Y-m-d H:i:s'); 
 
        
-    if($data->name){     
-        //echo $conn;    
+    if($data->name){    
+        
+        // Create token header as a JSON string
+$header = json_encode(['alg' => 'HS256', 'clientid' => 'bduatv2krl']);
+
+// {
+//     "mercid": “BDMERCID”,
+//     "": ,
+//     "": “300.00”,
+//     "": “”,
+//     "": “356”,
+//     "ru" : “https://merchant.com”,
+//     "": {
+//     "additional_info1": “Details1”,
+//     "additional_info2": “Details2”
+//     },
+//     "itemcode": “DIRECT”,
+//     "device": {
+//     "init_channel" : “internet”,
+//     "ip": “124.124.1.1”,
+//     "mac": “11-AC-58-21-1B-AA”,
+//     "imei": “990000112233445”,
+//     "accept_header": “text/html”,
+//     "fingerprintid": “61b12c18b5d0cf901be34a23ca64bb19”
+//     }
+//     }
+// Create token payload as a JSON string
+$payload = json_encode(['mercid' => 'BDUATV2KRL', 
+                        'orderid' => 'order45608988', 
+                        'amount' => 300.00, 
+                        'order_date' => '2021-03-05T10:59:15+05:30', 
+                        'currency' => 356, 
+                        'ru' => 'https://merchant.com', 
+                        'additional_info' => [
+                            'additional_info1' => 'Details1',
+                            'additional_info2' => 'Details1'
+                        ]
+                    ]);
+
+// Encode Header to Base64Url String
+$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+// Encode Payload to Base64Url String
+$base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+
+// Create Signature Hash
+$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'abC123!', true);
+
+// Encode Signature to Base64Url String
+$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+// Create JWT
+$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+echo $jwt;
+
         $stmt = $conn->prepare("
 		INSERT INTO items(`name`, `description`, `price`, `category_id`, `created`)
 		VALUES(?,?,?,?,?)");
 	
 	$name = htmlspecialchars(strip_tags($data->name));
-	$description = htmlspecialchars(strip_tags($data->description));
+	$description = $jwt;
 	$price = htmlspecialchars(strip_tags($data->price));
 	$category_id = htmlspecialchars(strip_tags($data->category_id));
 	$created = htmlspecialchars(strip_tags($data->created));
